79b3e22aa54850dcce1a00e93d929f79e267299a
adnanmutilovic <126587226+adnanmutilovic@users.noreply.github.com>
2023-06-20 18:06:59 +0200

    Delete cronJob.js

--- cronJob.js
+++
@@ -1,8 +0,0 @@
-const cron = require('node-cron');
-const { updateRates } = require('./rateUpdater');
-
-// Update rates every minute for testing
-cron.schedule('* * * * *', () => {
-  console.log('Updating currency rates...');
-  updateRates();
-});
